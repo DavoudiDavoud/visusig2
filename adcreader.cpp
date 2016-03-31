7a0f6a02328c239ac00f39937488700b5014b7b5
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-03-31 15:43:51 +0100

    Update adcreader.cpp

--- adcreader.cpp
+++ adcreader.cpp
@@ -1,5 +1,5 @@
 #include "adcreader.h"
-#include <QDebug>
+//#include <QDebug>
 #include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -25,6 +25,7 @@ static uint8_t bits = 8;
 static uint32_t speed = 50000;
 static uint16_t delay = 10;
 static int drdy_GPIO = 22;
+
 int inp=0;
 int outp=0;
 int buff[100];
@@ -32,18 +33,18 @@ int buff[100];
 static void writeReset(int fd)
 {
 	int ret;
+	
 	uint8_t tx1[5] = {0xff,0xff,0xff,0xff,0xff};
 	uint8_t rx1[5] = {0};
-	struct spi_ioc_transfer tr = {
-		(unsigned long)tx1,
-		(unsigned long)rx1,
-		ARRAY_SIZE(tx1),
-		speed,
-		delay,
-		bits,
-		NULL,
-		NULL,
-	};
+	
+	struct spi_ioc_transfer tr;
+  	tr.tx_buf = (unsigned long)tx1;
+  	tr.rx_buf = (unsigned long)rx1;
+  	tr.len = ARRAY_SIZE(tx1);
+  	tr.delay_usecs = delay;
+  	tr.speed_hz = speed;
+  	tr.bits_per_word = bits;
+  	
 	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
 	if (ret < 1)
 		pabort("can't send spi message");
@@ -52,19 +53,18 @@ static void writeReset(int fd)
 static void writeReg(int fd, uint8_t v)
 {
 	int ret;
+	
 	uint8_t tx1[1];
 	tx1[0] = v;
 	uint8_t rx1[1] = {0};
-	struct spi_ioc_transfer tr = {
-		(unsigned long)tx1,
-		(unsigned long)rx1,
-		ARRAY_SIZE(tx1),
-		speed,
-		delay,
-		bits,
-		NULL,NULL,
-
-	};
+  	
+  	struct spi_ioc_transfer tr;
+	tr.tx_buf = (unsigned long)tx1;
+	tr.rx_buf = (unsigned long)rx1;
+	tr.len = ARRAY_SIZE(tx1);
+	tr.delay_usecs = delay;
+	tr.speed_hz = speed;
+	tr.bits_per_word = bits;
 
 	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
 	if (ret < 1)
@@ -75,19 +75,18 @@ static void writeReg(int fd, uint8_t v)
 static uint8_t readReg(int fd)
 {
 	int ret;
+	
 	uint8_t tx1[1];
 	tx1[0] = 0;
 	uint8_t rx1[1] = {0};
-	struct spi_ioc_transfer tr = {
-		(unsigned long)tx1,
-		(unsigned long)rx1,
-		ARRAY_SIZE(tx1),
-		speed,
-		delay,
-		8,
-		NULL,NULL
-	};
-
+  	
+  	struct spi_ioc_transfer tr;
+	tr.tx_buf = (unsigned long)tx1;
+	tr.rx_buf = (unsigned long)rx1;
+	tr.len = ARRAY_SIZE(tx1);
+	tr.delay_usecs = delay;
+	tr.speed_hz = speed;
+	tr.bits_per_word = bits;
 
 	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
 	if (ret < 1)
@@ -99,16 +98,17 @@ static uint8_t readReg(int fd)
 static int readData(int fd)
 {
 	int ret;
+
 	uint8_t tx1[2] = {0,0};
 	uint8_t rx1[2] = {0,0};
-	struct spi_ioc_transfer tr = {
-		(unsigned long)tx1,
-		(unsigned long)rx1,
-		ARRAY_SIZE(tx1),
-		speed,
-		delay,
-		8,NULL,NULL,
-	};
+ 
+  	struct spi_ioc_transfer tr;
+	tr.tx_buf = (unsigned long)tx1;
+	tr.rx_buf = (unsigned long)rx1;
+	tr.len = ARRAY_SIZE(tx1);
+	tr.delay_usecs = delay;
+	tr.speed_hz = speed;
+	tr.bits_per_word = bits;
 
 	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
 	if (ret < 1)
